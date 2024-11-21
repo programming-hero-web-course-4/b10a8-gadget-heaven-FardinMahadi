83acc16ae8ab7776168620687aa47e2d2a067606
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-11-21 19:02:46 +0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const Banner = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Banner;
